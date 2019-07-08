986657ddfb3d1e3af283f599ee38c028e3b1f153
YuhanLiin <linyuhan0315@hotmail.com>
2019-07-08 19:32:35 -0400

    Finished structopt integration

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,25 +1,20 @@
-use clap::{App, AppSettings, Arg, SubCommand};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "kvs")]
+enum Kvs {
+    #[structopt(name = "get")]
+    Get { key: String },
+
+    #[structopt(name = "set")]
+    Set { key: String, value: String },
+
+    #[structopt(name = "rm")]
+    Remove { key: String },
+}
 
 pub fn main() {
-    let name = env!("CARGO_PKG_NAME");
-    let authors = env!("CARGO_PKG_AUTHORS");
-    let version = env!("CARGO_PKG_VERSION");
-    let description = env!("CARGO_PKG_DESCRIPTION");
-
-    let _m = App::new("kvs")
-        .name(name)
-        .author(authors)
-        .version(version)
-        .about(description)
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("KEY").required(true)))
-        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("KEY").required(true)))
-        .subcommand(
-            SubCommand::with_name("set")
-                .arg(Arg::with_name("KEY").required(true))
-                .arg(Arg::with_name("VALUE").required(true)),
-        )
-        .get_matches();
+    let args = Kvs::from_args();
 
     eprintln!("unimplemented");
     std::process::exit(1);
