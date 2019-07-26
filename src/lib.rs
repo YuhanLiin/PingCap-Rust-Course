a67cfcfc54234f8a3e498102b21ab7eee56e92b9
YuhanLiin <linyuhan0315@hotmail.com>
2019-07-26 01:46:37 -0400

    Started project 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
 #![deny(missing_docs)]
 //! Implements an in-memory key-value storage system.
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
+use std::path::Path;
+
+#[derive(Debug)]
+struct CustomErr;
+
+impl fmt::Display for CustomErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        panic!();
+    }
+}
+
+impl Error for CustomErr {}
+
+/// Custom Result type used for KvStore operations.
+pub type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 /// Key-value store for storing strings.
 /// rust ```
@@ -20,6 +37,11 @@ impl KvStore {
         Self(HashMap::new())
     }
 
+    /// Loads the in-memory index of the storage from a file to construct a KvStore
+    pub fn open(path: &Path) -> Result<Self> {
+        Ok(Self::new())
+    }
+
     /// Maps a key in the storage to a specific value.
     /// Overwrites previous value if the key already exists.
     /// rust ```
@@ -31,14 +53,15 @@ impl KvStore {
     ///     assert_eq!(kv.get("key".to_owned()), Some("2".to_owned()));
     /// # }
     /// ```
-    pub fn set(&mut self, key: String, val: String) {
+    pub fn set(&mut self, key: String, val: String) -> Result<()> {
         self.0.insert(key, val);
+        Ok(())
     }
 
     /// Returns a copy of the value mapped to a given key if it exists.
     /// Otherwise, return None.
-    pub fn get(&self, key: String) -> Option<String> {
-        self.0.get(&key).cloned()
+    pub fn get(&self, key: String) -> Result<Option<String>> {
+        Ok(self.0.get(&key).cloned())
     }
 
     /// Removes a key and its value from the storage.
@@ -52,7 +75,8 @@ impl KvStore {
     ///     assert_eq!(kv.get("key".to_owned()), None);
     /// # }
     /// ```
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.0.remove(&key);
+        Ok(())
     }
 }
