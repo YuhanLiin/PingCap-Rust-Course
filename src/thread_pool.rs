626cc8ebb123d99574886e45805fe3a0ee197051
YuhanLiin <linyuhan0315@hotmail.com>
2019-08-08 03:40:36 -0400

    Got rayon threadpools done

--- src/thread_pool.rs
+++ src/thread_pool.rs
@@ -1,5 +1,6 @@
 use crate::Result;
 use log::{error, info};
+use rayon;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 use std::thread::{spawn, JoinHandle};
@@ -42,6 +43,8 @@ impl SharedQueueThreadPool {
     fn new_thread(receiver: Arc<Mutex<Receiver<Job>>>, idx: u32) -> JoinHandle<()> {
         spawn(move || {
             loop {
+                // We only care about handling unwind panics, since abort panics end every thread
+                // anyways
                 if let Err(_) = std::panic::catch_unwind(|| {
                     let job = match receiver.lock().expect("mutex poisoned").recv() {
                         Ok(job) => job,
@@ -83,18 +86,22 @@ impl ThreadPool for SharedQueueThreadPool {
     }
 }
 
-/// TODO
-pub struct RayonThreadPool;
+/// Wraps rayon's threadpool
+pub struct RayonThreadPool(rayon::ThreadPool);
 
 impl ThreadPool for RayonThreadPool {
-    fn new(_threads: u32) -> Result<Self> {
-        Ok(Self)
+    fn new(threads: u32) -> Result<Self> {
+        Ok(RayonThreadPool(
+            rayon::ThreadPoolBuilder::new()
+                .num_threads(threads as usize)
+                .build()?,
+        ))
     }
 
     fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
     {
-
+        self.0.install(job);
     }
 }
